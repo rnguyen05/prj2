06db517d76c5e8d9272897bc3d0566783f28ccb9
saraalinazari <sara.alinazari@gmail.com>
2018-06-08 13:03:00 -0700

    m

--- controllers/properties_controller.js
+++ controllers/properties_controller.js
@@ -1,10 +1,20 @@
+
+
 var db  = require('../models');
 
 exports.index = function(req, res) {
-  console.log("in properties  controler");
+  console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$4");
+  let location = req.params.location;
+  //  let location = //"Irvine,CA";//req.body.location;
+  console.log("location",location);
+  let locationArr = location.split(",");
+  let city = locationArr[0];
+  let state = locationArr[1];
+
   db.Property.findAll({
     where: {
-      location: req.params.location
+      City: city,
+      State: state
     }
   }).then(function(dbProperty) {
     console.log(dbProperty);
--- public/assets/js/property.js
+++ public/assets/js/property.js
@@ -1,30 +1,63 @@
 
+$(document).ready(function() {
+// var url = window.location.search;
+// $("#propertySearchForm").on("submit", handleFormSubmit);
+// $("#locationSend").on("click", handleFormSubmit);
 
-var url = window.location.search;
-
-
-function handleFormSubmit(event) {
+// function handleFormSubmit(event) {
+  $("#locationSend").on("click",function(event){
+    window.location.href = "/properties/location";
     console.log("*************Location*****");
     // const currentURL = window.location.origin;
-   var location = $("#location");
+   var location = $("#location").val().trim();
     console.log("*************Location***********",location);
-    let locationUrl = "/?location="+location;
-     $.get("/properties/location" + locationUrl, function(data) {
-       console.log("*************Posts", data);
-       // property = data;
-       // if (!posts || !posts.length) {
-       //   displayEmpty(author);
-       // }
-       // else {
-       //   initializeRows();
-       // }
-     });
+    let locationUrl = "?lo="+location;
+    alert(locationUrl);
+    //  $.get("/properties/location" , locationUrl, function(data) {
+    //    console.log("*************Posts", data);
+    //    // property = data;
+    //    // if (!posts || !posts.length) {
+    //    //   displayEmpty(author);
+    //    // }
+    //    // else {
+    //    //   initializeRows();
+    //    // }
+    //  });
+  $.get("/properties/location" + locationUrl);
+    // $.get("/properties/location",{location:location});
+    });
+  });
             // $.get("/properties/:"+location, function(data) {
             //     location = data;
             //     // initializeRows();
             //   });
             
-}
+//}
 
-// $("#propertySearchForm").on("submit", handleFormSubmit);
-$("#locationSend").on("click", handleFormSubmit);
+// $("#propertySearchForm").on("submit", handleFormSu
+// });
+
+//     console.log("*************Location*****");
+//     // const currentURL = window.location.origin;
+//    var location = $("#location");
+//     console.log("*************Location***********",location.toString());
+//     let locationUrl = "/?location="+location;
+//      $.get("/properties/location" + locationUrl, function(data) {
+//        console.log("*************Posts", data);
+//        // property = data;
+//        // if (!posts || !posts.length) {
+//        //   displayEmpty(author);
+//        // }
+//        // else {
+//        //   initializeRows();
+//        // }
+//      });
+//             // $.get("/properties/:"+location, function(data) {
+//             //     location = data;
+//             //     // initializeRows();
+//             //   });
+            
+// }
+
+// // $("#propertySearchForm").on("submit", handleFormSu
+// });
